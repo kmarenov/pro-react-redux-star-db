9ecf5b9d704099cc7682254d0fff11cc94452a15
Kirill Marenov <kmarenov@gmail.com>
2020-02-17 14:10:10 +0300

    67. Используем на практике componentDidUpdate()

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,7 +10,7 @@ import './app.css';
 export default class App extends Component {
     state = {
         showRandomPlanet: true,
-        selectedPerson: null
+        selectedPerson: 4
     };
 
     toggleRandomPlanet = () => {
