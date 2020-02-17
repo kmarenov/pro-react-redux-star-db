10232d090447ed1802a15ec33a9696f27738a14a
Kirill Marenov <kmarenov@gmail.com>
2020-02-17 17:20:14 +0300

    70. Используем componentDidCatch() на практике

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -46,8 +46,6 @@ export default class App extends Component {
                 </button>
 
                 <PeoplePage />
-                <PeoplePage />
-                <PeoplePage />
             </div>
         );
     }
