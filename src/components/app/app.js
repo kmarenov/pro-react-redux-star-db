2f40fbca8b389947ca761b66048e40a08efa2f8b
Kirill Marenov <kmarenov@gmail.com>
2020-02-17 12:33:45 +0300

    65. Используем на практике componentDidMount()

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,7 +9,8 @@ import './app.css';
 
 export default class App extends Component {
     state = {
-        showRandomPlanet: true
+        showRandomPlanet: true,
+        selectedPerson: null
     };
 
     toggleRandomPlanet = () => {
@@ -18,6 +19,10 @@ export default class App extends Component {
                 showRandomPlanet: !state.showRandomPlanet
             }
         });
+    };
+
+    onPersonSelected = (id) => {
+      this.setState({ selectedPerson: id });
     };
 
     render() {
@@ -38,10 +43,10 @@ export default class App extends Component {
 
                 <div className="row mb2">
                     <div className="col-md-6">
-                        <ItemList />
+                        <ItemList onItemSelected={this.onPersonSelected} />
                     </div>
                     <div className="col-md-6">
-                        <PersonDetails />
+                        <PersonDetails personId={this.state.selectedPerson} />
                     </div>
                 </div>
             </div>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -47,7 +47,7 @@ export default class SwapiService {
         return item.url.match(idRegExp)[1];
     }
 
-    _transformPlanet(planet) {
+    _transformPlanet = (planet) => {
         return {
             id: this._extractId(planet),
             name: planet.name,
@@ -55,9 +55,9 @@ export default class SwapiService {
             rotationPeriod: planet.rotation_period,
             diameter: planet.diameter,
         };
-    }
+    };
 
-    _transformStarship(starship) {
+    _transformStarship = (starship) => {
         return {
             id: this._extractId(starship),
             name: starship.name,
@@ -69,9 +69,9 @@ export default class SwapiService {
             passengers: starship.passengers,
             cargoCapacity: starship.cargoCapacity
         }
-    }
+    };
 
-    _transformPerson(person) {
+    _transformPerson = (person) => {
         return {
             id: this._extractId(person),
             name: person.name,
@@ -79,5 +79,5 @@ export default class SwapiService {
             birthYear: person.birthYear,
             eyeColor: person.eyeColor
         }
-    }
+    };
 }
