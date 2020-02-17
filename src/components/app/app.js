9e710eb3bdc55e35dedd241b0ca377027e2aa850
Kirill Marenov <kmarenov@gmail.com>
2020-02-17 16:57:09 +0300

    70. Используем componentDidCatch() на практике

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 
 import './app.css';
 import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page/people-page";
 
 export default class App extends Component {
     state = {
         showRandomPlanet: true,
-        selectedPerson: 4,
         hasError: false
     };
 
@@ -25,10 +23,6 @@ export default class App extends Component {
                 showRandomPlanet: !state.showRandomPlanet
             }
         });
-    };
-
-    onPersonSelected = (id) => {
-      this.setState({ selectedPerson: id });
     };
 
     render() {
@@ -51,14 +45,9 @@ export default class App extends Component {
                     Toggle Random Planet
                 </button>
 
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={this.onPersonSelected} />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
-                </div>
+                <PeoplePage />
+                <PeoplePage />
+                <PeoplePage />
             </div>
         );
     }
