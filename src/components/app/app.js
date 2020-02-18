3ceb92290c9b52aa152021d75d5fdadb840b5ef5
Kirill Marenov <kmarenov@gmail.com>
2020-02-18 10:14:21 +0300

    73. Render-функции

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -56,7 +56,20 @@ export default class App extends Component {
                     <div className="col-md-6">
                         <ItemList
                             onItemSelected={this.onPersonSelected}
-                            getData={this.swapiService.getAllPlanets} />
+                            getData={this.swapiService.getAllPlanets}
+                            renderItem={(item) => item.name} />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList
+                            onItemSelected={this.onPersonSelected}
+                            getData={this.swapiService.getAllStarships}
+                            renderItem={(item) => item.name} />
                     </div>
                     <div className="col-md-6">
                         <PersonDetails personId={this.state.selectedPerson} />
