47eaa11ed82d6fcb7e05081316a563d32cccda3c
Kirill Marenov <kmarenov@gmail.com>
2020-02-19 13:57:12 +0300

    87. Рефакторинг: делаем код чище

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import './app.css';
-import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page/people-page';
 import {PlanetDetails, PlanetList, StarshipDetails, StarshipList} from '../sw-components';
 import Row from '../row';
@@ -11,46 +10,14 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 
 export default class App extends Component {
-    state = {
-        showRandomPlanet: true,
-        hasError: false
-    };
-    
-    swapiService = new SwapiService();
-
-    componentDidCatch(error, errorInfo) {
-        this.setState({ hasError: true });
-    }
-
-    toggleRandomPlanet = () => {
-        this.setState((state) => {
-            return {
-                showRandomPlanet: !state.showRandomPlanet
-            }
-        });
-    };
-
-    render() {
-        if (this.state.hasError) {
-            return <ErrorIndicator />;
-        }
-
-        const planet = this.state.showRandomPlanet ?
-            <RandomPlanet/> :
-            null;
-
+   render() {
         return (
             <ErrorBoundry>
-                <SwapiServiceProvider value={this.swapiService}>
+                <SwapiServiceProvider value={new SwapiService()}>
                     <div className="stardb-app">
                         <Header />
-                        { planet }
 
-                        <button
-                            className="toggle-planet btn btn-warning btn-lg"
-                            onClick={this.toggleRandomPlanet}>
-                            Toggle Random Planet
-                        </button>
+                        <RandomPlanet/>
 
                         <PeoplePage />
 
