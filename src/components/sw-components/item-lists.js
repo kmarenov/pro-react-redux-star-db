405a030186bb5ff7089712bd3a11af360ee07591
Kirill Marenov <kmarenov@gmail.com>
2020-02-19 18:17:09 +0300

    89. Функция compose() (опциональный урок))

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import {withData, withSwapiService} from '../hoc-helpers';
+import {compose, withData, withSwapiService, withChildFunction} from '../hoc-helpers';
 import ItemList from '../item-list';
-
-const withChildFunction = (fn) => (Wrapped) => {
-    return (props) => {
-        return (
-            <Wrapped {...props}>
-                {fn}
-            </Wrapped>
-        )
-    }
-};
 
 const mapPersonMethodsToProps = (swapiService) => {
     return { getData: swapiService.getAllPeople };
@@ -24,9 +14,23 @@ const mapStarshipMethodsToProps = (swapiService) => {
     return { getData: swapiService.getAllStarships };
 };
 
-const PersonList = withSwapiService(mapPersonMethodsToProps)(withData(withChildFunction(i => `${i.name} (${i.birthYear})`)(ItemList)));
-const PlanetList = withSwapiService(mapPlanetMethodsToProps)(withData(withChildFunction(item => item.name)(ItemList)));
-const StarshipList = withSwapiService(mapStarshipMethodsToProps)(withData(withChildFunction(({model, name}) => <span>{name} ({model})</span>)(ItemList)));
+const PersonList = compose(
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(i => `${i.name} (${i.birthYear})`)
+)(ItemList);
+
+const PlanetList = compose(
+    withSwapiService(mapPlanetMethodsToProps),
+    withData,
+    withChildFunction(item => item.name)
+)(ItemList);
+
+const StarshipList = compose(
+    withSwapiService(mapStarshipMethodsToProps),
+    withData,
+    withChildFunction(({model, name}) => <span>{name} ({model})</span>)
+)(ItemList);
 
 export {
     PersonList,
