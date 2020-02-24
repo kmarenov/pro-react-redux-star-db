5f9ac3019502fa0ae39e62801a8b04769823018b
Kirill Marenov <kmarenov@gmail.com>
2020-02-24 22:40:48 +0300

    107. Как работает Route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,8 @@ export default class App extends Component {
                         <Header />
                         <RandomPlanet />
 
+                        <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
+                        <Route path="/people" render={() => <h2>People</h2>} />
                         <Route path="/people" component={PeoplePage} />
                         <Route path="/planets" component={PlanetsPage} />
                         <Route path="/starships" component={StarshipsPage} />
