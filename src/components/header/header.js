8eda6f148177cd7f23d640360bc57daf7f3f628a
Kirill Marenov <kmarenov@gmail.com>
2020-02-24 22:29:58 +0300

    106. Link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
@@ -12,13 +13,13 @@ const Header = () => {
       </h3>
       <ul className="d-flex">
         <li>
-          <a href="#">People</a>
+            <Link to="/people">People</Link>
         </li>
         <li>
-          <a href="#">Planets</a>
+            <Link to="/planets">Planets</Link>
         </li>
         <li>
-          <a href="#">Starships</a>
+            <Link to="/starships">Starships</Link>
         </li>
       </ul>
     </div>
